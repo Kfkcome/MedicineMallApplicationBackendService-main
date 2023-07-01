8464be1ccb919e05a26c11c30dd8eecce519110b
Kfkcome <killzion666@gmail.com>
2023-07-01 14:52:03 +0800

    feat(controller mapper service application domain): 初始化项目结构
    完成登录功能的接口
    
    Closes 购物接口 个人信息完善

---
+++ src/main/java/pers/ervinse/MedicinesMallApplicationBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package pers.ervinse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinesMallApplicationBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinesMallApplicationBackendServiceApplication.class, args);
+    }
+
+}
